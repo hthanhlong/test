940302eda3639d61a7ba701b058d2d9fcb0e55c4
hthanhlong97 <hthanhlong97@gmail.com>
2023-08-06 22:59:03 +0700

    init errorhandler

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,10 @@
-import express, { Application } from 'express'
+import express, {
+    Application,
+    Errback,
+    NextFunction,
+    Request,
+    Response,
+} from 'express'
 import ENV_CONGIF from './constant'
 import { initDatabase } from './database'
 
@@ -7,6 +13,25 @@ const PORT = ENV_CONGIF.PORT
 const main = async () => {
     await initDatabase()
     const app: Application = express()
+
+    app.use('/', (req: Request, res: Response): void => {
+        res.send('Hello wolrd')
+    })
+
+    app.use(
+        (
+            error: Errback,
+            req: Request,
+            res: Response,
+            next: NextFunction
+        ): void => {
+            if (res.headersSent) {
+                return next(error)
+            }
+            res.status(500)
+            res.render('error', { error: error })
+        }
+    )
 
     app.listen(PORT, () => {
         console.log(`server already running --> ${PORT}`)
